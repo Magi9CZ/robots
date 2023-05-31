a76a8afad6195c0ccf50b40dcf2ca2278a7b9f30
Michal Jiránek <michal.jiranek00@gmail.com>
2023-06-01 01:03:05 +0200

    main

---
+++ src/components/GameBoard.js
@@ -0,0 +1,203 @@
+import React, {useEffect, useReducer, useState} from "react";
+import "./GBstyles.css";
+
+function GameBoard() {
+
+    const initialBoard = [
+        [1, 1, 1, 1, 1, 1, 2, 0, 0],
+        [1, 1, 1, 0, 1, 0, 1, 0, 1],
+        [1, 0, 0, 0, 0, 0, 0, 0, 0],
+        [1, 1, 0, 1, 1, 1, 1, 1, 1],
+        [1, 0, 0, 0, 0, 0, 0, 0, 0],
+        [1, 1, 1, 1, 1, 1, 1, 0, 1],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [1, 1, 0, 1, 1, 1, 1, 1, 1],
+        [1, 0, 0, 0, 0, 0, 0, 3, 1],
+    ];
+
+    const [board, setBoard] = useState(initialBoard);
+    const [inputOne, setInputOne] = useState("");
+    const [inputTwo, setInputTwo] = useState("");
+    const [ignored, forceUpdate] = useReducer(x => x + 1, 0)
+    let gameOver = false
+    let i = 500;
+    let firstRobotInitialPosition;
+    let secondRobotInitialPosition;
+    let isFirstFinished = false
+    let isSecondFinished = false
+
+    function handleInputOne(e) {
+        setInputOne(e.target.value);
+    }
+
+    function handleInputTwo(e) {
+        setInputTwo(e.target.value);
+    }
+
+    function startGame() {
+        firstRobotInitialPosition = najitPoziciPrvku(board, 2);
+        secondRobotInitialPosition = najitPoziciPrvku(board, 3)
+        moveRobots();
+    }
+
+    function najitPoziciPrvku(dvourozmernyPole, hledanyPrvek) {
+        for (let i = 0; i < dvourozmernyPole.length; i++) {
+            for (let j = 0; j < dvourozmernyPole[i].length; j++) {
+                if (dvourozmernyPole[i][j] === hledanyPrvek) {
+                    return {radek: i, sloupec: j};
+                }
+            }
+        }
+        return null; // Prvek nebyl nalezen
+    }
+
+    function moveRobots() {
+        const firstPath = inputOne.toUpperCase().split('')
+        const secondPath = inputTwo.toUpperCase().split('')
+        moveRobot(firstPath, 2)
+        moveRobot(secondPath, 3)
+        const timeoutTime =  secondPath.length > firstPath.length ? secondPath.length : firstPath.length
+        setTimeout(() => {
+            console.log(isFirstFinished, isSecondFinished)
+        }, i * timeoutTime +500)
+
+    }
+
+    function moveRobot(path, numOfRobot) {
+        const opositeRobot = numOfRobot === 2 ? 3 : 2;
+        const opositeRobotStartingPosition = numOfRobot === 2 ? secondRobotInitialPosition : firstRobotInitialPosition
+        let x = 0;
+        path.forEach(value => {
+            switch (value) {
+                case 'V':
+                    x++;
+                    setTimeout(() => {
+                        const position = najitPoziciPrvku(board, numOfRobot)
+                        if (board[position.radek][position.sloupec + 1] !== 0 && board[position.radek][position.sloupec + 1] !== opositeRobot || gameOver) {
+                            alert("GameOver");
+                            gameOver = true
+                        }
+                        else {
+                            const newBoard = board
+                            newBoard[position.radek][position.sloupec] = 0
+                            newBoard[position.radek][position.sloupec + 1] = numOfRobot
+                            setBoard(newBoard)
+                            forceUpdate()
+                            const currentPosition = najitPoziciPrvku(board, numOfRobot)
+                            if(currentPosition.radek === opositeRobotStartingPosition.radek && currentPosition.sloupec === opositeRobotStartingPosition.sloupec){
+                                numOfRobot ===2 ? isFirstFinished = true : isSecondFinished = true
+                            }else{
+                                numOfRobot ===2 ? isFirstFinished = false : isSecondFinished = false
+                            }
+                        }
+
+                    }, i * x)
+
+                    break;
+                case 'Z':
+                    x++;
+                    setTimeout(() => {
+                        const position = najitPoziciPrvku(board, numOfRobot)
+                        if (board[position.radek][position.sloupec - 1] !== 0 && board[position.radek][position.sloupec - 1] !== opositeRobot || gameOver) {
+                            alert("GameOver");
+                            gameOver = true
+                        } else {
+                            const newBoard = board
+                            newBoard[position.radek][position.sloupec] = 0
+                            newBoard[position.radek][position.sloupec - 1] = numOfRobot
+                            setBoard(newBoard)
+                            forceUpdate()
+                            const currentPosition = najitPoziciPrvku(board, numOfRobot)
+                            if(currentPosition.radek === opositeRobotStartingPosition.radek && currentPosition.sloupec === opositeRobotStartingPosition.sloupec){
+                                numOfRobot ===2 ? isFirstFinished = true : isSecondFinished = true
+                            }else{
+                                numOfRobot ===2 ? isFirstFinished = false : isSecondFinished = false
+                            }
+                        }
+
+                    }, i * x)
+
+                    break;
+                case 'J':
+                    x++;
+                    setTimeout(() => {
+                        const position = najitPoziciPrvku(board, numOfRobot)
+                        if (board[position.radek + 1][position.sloupec] !== 0 && board[position.radek + 1][position.sloupec] !== opositeRobot || gameOver) {
+                            alert("GameOver");
+                            gameOver = true
+                        } else {
+                            const newBoard = board
+                            newBoard[position.radek][position.sloupec] = 0
+                            newBoard[position.radek + 1][position.sloupec] = numOfRobot
+                            setBoard(newBoard)
+                            forceUpdate()
+                            const currentPosition = najitPoziciPrvku(board, numOfRobot)
+                            if(currentPosition.radek === opositeRobotStartingPosition.radek && currentPosition.sloupec === opositeRobotStartingPosition.sloupec){
+                                numOfRobot ===2 ? isFirstFinished = true : isSecondFinished = true
+                            }else{
+                                numOfRobot ===2 ? isFirstFinished = false : isSecondFinished = false
+                            }
+                        }
+
+                    }, i * x)
+
+                    break;
+                case 'S':
+                    x++;
+                    setTimeout(() => {
+                        const position = najitPoziciPrvku(board, numOfRobot)
+                        if (board[position.radek - 1][position.sloupec] !== 0 && board[position.radek - 1][position.sloupec] !== opositeRobot || gameOver) {
+                            alert("GameOver");
+                            gameOver = true
+                        } else {
+                            const newBoard = board
+                            newBoard[position.radek][position.sloupec] = 0
+                            newBoard[position.radek - 1][position.sloupec] = numOfRobot
+                            setBoard(newBoard)
+                            forceUpdate()
+                            const currentPosition = najitPoziciPrvku(board, numOfRobot)
+                            if(currentPosition.radek === opositeRobotStartingPosition.radek && currentPosition.sloupec === opositeRobotStartingPosition.sloupec){
+                                numOfRobot ===2 ? isFirstFinished = true : isSecondFinished = true
+                            }else{
+                                numOfRobot ===2 ? isFirstFinished = false : isSecondFinished = false
+                            }
+                        }
+
+                    }, i * x)
+
+                    break;
+            }
+        })
+    }
+
+
+    return (
+        <div className="game-board">
+            <div>
+                <label>Robot 1 </label>
+                <input type={"text"} onChange={handleInputOne}/>
+            </div>
+            <div>
+                <label>Robot 2 </label>
+                <input type={"text"} onChange={handleInputTwo}/>
+            </div>
+            <div>
+                <button type={"submit"} onClick={startGame}>Zahájit hru</button>
+            </div>
+            <div>
+                {board.map((row, rowIndex) => (
+                    <div className="board-row" key={rowIndex}>
+                        {row.map((col, colIndex) => (
+                            <div
+                                className={`square ${col === 0 ? 'gray' : col === 1 ? 'white' : col === 2 ? 'green' : 'red'}`}
+                                key={`${rowIndex}-${colIndex}`}
+                            ></div>
+                        ))}
+                    </div>
+                ))}
+            </div>
+        </div>
+    )
+}
+
+export default GameBoard;
